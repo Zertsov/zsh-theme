ff63b43a1f3cab3152c23455a8a1e5ff9b126c9f
Mitch Vostrez <mitchelvostrez@gmail.com>
2023-09-25 01:36:44 -0500

    Make setup script more robust and cleaner

--- setup.sh
+++ setup.sh
@@ -1,39 +1,82 @@
 #!/bin/bash
 
-# Make sure we have either curl or wget - you GOTTA have one
-if [ -x "$(command -v curl)" ]; then
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
-
-	# Install brew if we're OSX (which we probably are)
-	if [[ $OSTYPE == 'darwin'* || $OSTYPE == 'linux-gnu'* ]]; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-	fi
-
-
-# Assume that if we have wget then we have Brew, since I can't think of a reasonable time you don't
-# have curl and you do have wget
-elif [ -x "$(command -v wget)" ]; then
-  wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+# Ensure the system has either curl or wget
+if ! check_command_installed curl && ! check_command_installed wget; then
+		echo "Error: This script requires either 'curl' or 'wget' to be installed."
+		exit 1
 fi
 
-cp .zshrc ~/.zshrc
-cp .alias ~/.alias
-cp .funcs ~/.funcs
+check_command_installed () { command -v "$1" &>/dev/null; }
 
-# Install ripgrep
-brew install ripgrep
+log_install () {
+	echo "[install]: $1"
+}
 
-# xcode tools is dumb - gotta do this first so we can have git
+if check_command_installed "curl"; then
+		DLCMD="curl -fsSL"
+else
+		DLCMD="wget -qO-"
+fi
+
+install_nvm () {
+	log_install "Installing NVM..."
+	$DLCMD https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+}
+
+install_brew () {
+	log_install "Installing Homebrew..."
+	bash -c "$($DLCMD https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+}
+
+install_ripgrep () {
+	log_install "Installing ripgrep..."
+	brew install ripgrep
+}
+
+install_nvim () {
+	log_install "Installing nvim..."
+	brew install nvim
+	mkdir -p $HOME/.config/nvim
+	git clone git@github.com:Zertsov/nvim.git ~/.config/nvim
+}
+
+##############################################################
+# Begin installs																					   #
+# We assume Git is installed in all systems that aren't OSX. #
+##############################################################
+
+# OSX is dumb - gotta do this first so we can have git
 if [[ $OSTYPE == 'darwin'* ]]; then
 	xcode-select --install
 fi
 
-if ! [ -x "$(command -v git)" ]; then
-	echo "Can't set up nvim - can't find git executable"
+# Get nvm
+if ! check_command_installed "nvm"; then
+	install_nvm
 fi
 
-# Time to get NVIM
-# TODO: Don't raw dog brew install here since we're trying to make this work for linux too
-brew install nvim
-mkdir -p $HOME/.config/nvim
-git clone git@github.com:Zertsov/nvim.git ~/.config/nvim
+# Get brew
+if ! check_command_installed "brew"; then
+	install_brew
+fi
+
+# Get ripgrep
+if ! check_command_installed "rg"; then
+	install_ripgrep
+fi
+
+# Get NVIM
+if ! check_command_installed "nvim"; then
+	install_nvim
+fi
+
+# Override local dotfiles with our own
+cp .zshrc ~/.zshrc
+cp .alias ~/.alias
+cp .funcs ~/.funcs
+
+if ! [ -x "$(command -v git)" ]; then
+	echo "Can't set up nvim - can't find git executable"
+	exit 1
+fi
+
